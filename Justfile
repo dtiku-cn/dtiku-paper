bb4b3be9b1b4de900511bcfef8f001846ea5a120
holmofy <hff1996723@163.com>
2024-10-30 20:31:45 +0800

    nav

---
+++ Justfile
@@ -0,0 +1,3 @@
+## generate model
+gen-model:
+    sea-orm-cli generate entity --with-serde="both" --output-dir="dtiku-core/src/model/_entities" --enum-extra-derives="strum::EnumString" --enum-extra-attributes="serde(rename_all = \"snake_case\")"
--- ddl/paper.sql
+++ ddl/paper.sql
@@ -1,40 +1,43 @@
+create extension if not exists vector;
 create table if not exists label (
     id serial primary key,
     name varchar(32) not null,
-    pid integer not null,
+    pid integer not null
 );
 create table if not exists paper(
     id serial primary key,
     title varchar(255) not null,
     descrp text default null,
     label_id integer not null,
-    extra jsonb not null,
+    extra jsonb not null
 );
 create table if not exists question(
     id serial primary key,
     content text not null,
     extra jsonb not null,
+    embedding vector(512) not null
 );
 create table if not exists paper_question (
     paper_id integer not null,
     question_id integer not null,
-    sort: smallint not null,
+    sort smallint not null
 );
 create table if not exists material (
     id serial primary key,
     content text not null,
+    embedding vector(512) not null
 );
 create table if not exists paper_material (
     paper_id integer not null,
     material_id integer not null,
-    sort smallint not null,
+    sort smallint not null
 );
 create table if not exists question_material (
     question_id integer not null,
-    material_id integer not null,
+    material_id integer not null
 );
 create table if not exists solution (
     id serial primary key,
     question_id integer not null,
-    extra jsonb not null,
+    extra jsonb not null
 );
