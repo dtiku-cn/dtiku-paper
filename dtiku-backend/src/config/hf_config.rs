4eaf4f91c46d87b9298166707e8a0b646a2798f3
holmofy <hff1996723@163.com>
2024-12-11 14:35:22 +0800

    hugging face cache dir

--- dtiku-backend/src/config/hf_config.rs
+++ dtiku-backend/src/config/hf_config.rs
@@ -4,5 +4,10 @@ use spring::config::Configurable;
 #[derive(Debug, Deserialize, Configurable)]
 #[config_prefix = "huggingface"]
 pub struct HfConfig {
+    #[serde(default = "default_cache_dir")]
     pub cache_dir: String,
+}
+
+fn default_cache_dir() -> String {
+    "../.hf-cache".to_string()
 }
