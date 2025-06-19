40367ed34e962f34a21af3ecff651bf1cd02febb
holmofy <hff1996723@163.com>
2025-06-19 20:02:45 +0800

    fix label & exam_category

--- dtiku-paper/src/service/exam_category.rs
+++ dtiku-paper/src/service/exam_category.rs
@@ -5,6 +5,7 @@ use crate::{
 use itertools::Itertools;
 use sea_orm::DbConn;
 use spring::plugin::service::Service;
+use spring_redis::cache;
 
 #[derive(Clone, Service)]
 pub struct ExamCategoryService {
@@ -20,14 +21,18 @@ impl ExamCategoryService {
         ExamCategory::find_by_root_prefix(&self.db, prefix).await
     }
 
-    // #[cached(key = "paper_types:{prefix}")]
+    #[cache("paper_types:{root_id}", expire = 86400)]
     pub async fn find_leaf_paper_types(&self, root_id: i16) -> anyhow::Result<Vec<ExamPaperType>> {
         let ecs = ExamCategory::find_all_by_pid(&self.db, root_id).await?;
         let pids: Vec<i16> = ecs.iter().map(|ec| ec.id).collect();
         let leaf = ExamCategory::find_all_by_pids(&self.db, pids).await?;
-        let mut grouped = leaf.into_iter().into_group_map_by(|m| m.pid);
+        let mut grouped = leaf
+            .into_iter()
+            .sorted_by(|a, b| Ord::cmp(&a.id, &b.id))
+            .into_group_map_by(|m| m.pid);
         Ok(ecs
             .into_iter()
+            .sorted_by(|a, b| Ord::cmp(&a.id, &b.id))
             .map(|m| ExamPaperType::new(grouped.remove(&m.id), m))
             .collect())
     }
--- dtiku-paper/src/service/label.rs
+++ dtiku-paper/src/service/label.rs
@@ -5,6 +5,7 @@ use crate::{
 use itertools::Itertools;
 use sea_orm::DbConn;
 use spring::plugin::service::Service;
+use spring_redis::cache;
 
 #[derive(Clone, Service)]
 pub struct LabelService {
@@ -13,6 +14,7 @@ pub struct LabelService {
 }
 
 impl LabelService {
+    #[cache("label:tree:{paper_type}", expire = 86400)]
     pub async fn find_all_label_by_paper_type(&self, paper_type: i16) -> anyhow::Result<LabelTree> {
         let root_pid = 0;
         let ls = Label::find_all_by_query(
@@ -26,9 +28,13 @@ impl LabelService {
         let lids = ls.iter().map(|l| l.id).collect_vec();
         let labels = Label::find_by_paper_type_and_pids(&self.db, paper_type, lids).await?;
         let level = !labels.is_empty();
-        let mut grouped = labels.into_iter().into_group_map_by(|m| m.pid);
+        let mut grouped = labels
+            .into_iter()
+            .sorted_by(|a, b| Ord::cmp(&a.id, &b.id))
+            .into_group_map_by(|m| m.pid);
         let nodes = ls
             .into_iter()
+            .sorted_by(|a, b| Ord::cmp(&a.id, &b.id))
             .map(|m| LabelNode::new(grouped.remove(&m.id), m))
             .collect();
         Ok(LabelTree {
