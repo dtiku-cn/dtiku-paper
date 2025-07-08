731193fbc1ed45d4728b61f75bd446505ca6b7e9
holmofy <hff1996723@163.com>
2025-07-08 17:58:56 +0800

    fix home page idiom

--- dtiku-web/src/router/home.rs
+++ dtiku-web/src/router/home.rs
@@ -2,6 +2,10 @@ use crate::views::{home::HomeTemplate, GlobalVariables};
 use anyhow::Context;
 use askama::Template;
 use dtiku_paper::{model::paper, service::paper::PaperService};
+use dtiku_stats::{
+    model::sea_orm_active_enums::IdiomType, query::IdiomQuery, service::idiom::IdiomService,
+};
+use spring_sea_orm::pagination::Pagination;
 use spring_web::{
     axum::{
         response::{Html, IntoResponse},
@@ -15,14 +19,23 @@ use spring_web::{
 #[get("/")]
 async fn home(
     Component(ps): Component<PaperService>,
+    Component(is): Component<IdiomService>,
     Extension(global): Extension<GlobalVariables>,
 ) -> Result<impl IntoResponse> {
+    let query = &IdiomQuery {
+        label_id: vec![],
+        page: Pagination { page: 1, size: 10 },
+    };
     let xingce = get_papers(&ps, &global, "xingce").await?;
     let shenlun = get_papers(&ps, &global, "shenlun").await?;
+    let idioms = is.get_idiom_stats(IdiomType::Idiom, query).await?;
+    let words = is.get_idiom_stats(IdiomType::Word, query).await?;
     let t = HomeTemplate {
         global,
         xingce,
         shenlun,
+        idioms: idioms.content,
+        words: words.content,
     };
     Ok(Html(t.render().context("render failed")?))
 }
--- dtiku-web/src/views/home.rs
+++ dtiku-web/src/views/home.rs
@@ -1,6 +1,7 @@
 use super::GlobalVariables;
 use askama::Template;
 use dtiku_paper::model::paper;
+use dtiku_stats::domain::IdiomStats;
 
 #[derive(Template)]
 #[template(path = "home.html.min.jinja")]
@@ -8,4 +9,6 @@ pub struct HomeTemplate {
     pub global: GlobalVariables,
     pub xingce: Vec<paper::Model>,
     pub shenlun: Vec<paper::Model>,
+    pub idioms: Vec<IdiomStats>,
+    pub words: Vec<IdiomStats>,
 }
