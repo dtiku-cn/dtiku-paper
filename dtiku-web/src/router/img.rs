dbdcecf44a4323a966cb113a447e80cd604f26db
holmofy <hff1996723@163.com>
2025-08-23 14:00:04 +0800

    add upload-by-link

--- dtiku-web/src/router/img.rs
+++ dtiku-web/src/router/img.rs
@@ -2,10 +2,12 @@ use crate::{plugins::OpenListConfig, rpc};
 use anyhow::Context;
 use axum_extra::extract::Multipart;
 use chrono::{Datelike, Local};
+use serde::Deserialize;
+use serde_json::json;
 use spring::tracing;
 use spring_opendal::Op;
 use spring_web::{
-    axum::response::IntoResponse,
+    axum::{response::IntoResponse, Json},
     error::{KnownWebError, Result},
     extractor::{Component, Config},
     post,
@@ -27,13 +29,7 @@ async fn upload(
         let data = field.bytes().await.unwrap();
 
         if name == "file" {
-            let now = Local::now();
-            let year = now.year();
-            let month = now.month();
-            let day = now.day();
-            let dir_path = format!("pan.wo/artalk/{}/{:02}/{:02}", year, month, day);
-            let uid = Uuid::new_v4();
-            let file_path = format!("{dir_path}/{uid}");
+            let (dir_path, file_path) = dir_and_file_path();
             dav.create_dir(&format!("{dir_path}/"))
                 .await
                 .with_context(|| format!("mkdir for {dir_path} failed"))?;
@@ -49,4 +45,56 @@ async fn upload(
         }
     }
     Err(KnownWebError::bad_request("上传请求不正确").into())
+}
+
+fn dir_and_file_path() -> (String, String) {
+    let now = Local::now();
+    let year = now.year();
+    let month = now.month();
+    let day = now.day();
+    let dir_path = format!("pan.wo/artalk/{}/{:02}/{:02}", year, month, day);
+    let uid = Uuid::new_v4();
+    let file_path = format!("{dir_path}/{uid}");
+    (dir_path, file_path)
+}
+
+#[derive(Debug, Deserialize)]
+struct UploadUrlReq {
+    pub url: String,
+}
+
+#[post("/upload-by-link")]
+async fn upload_by_link(
+    Component(dav): Component<Op>,
+    Config(config): Config<OpenListConfig>,
+    Json(url_req): Json<UploadUrlReq>,
+) -> Result<impl IntoResponse> {
+    let original_url = url_req.url;
+    let resp = reqwest::get(&original_url)
+        .await
+        .with_context(|| format!("图片请求失败:{original_url}"))?;
+    let data = resp
+        .bytes()
+        .await
+        .with_context(|| format!("图片下载失败:{original_url}"))?;
+    let (dir_path, file_path) = dir_and_file_path();
+    dav.create_dir(&format!("{dir_path}/"))
+        .await
+        .with_context(|| format!("mkdir for {dir_path} failed"))?;
+    let resp = dav
+        .write(&file_path, data)
+        .await
+        .with_context(|| format!("upload to {file_path} failed"))?;
+    tracing::info!("upload ==> {resp:?}");
+    let url = rpc::alist::get_file_path(&file_path, &config)
+        .await
+        .with_context(|| format!("get_file_info({file_path}) failed"))?;
+    Ok(Json(json!({
+        "msg": "上传成功",
+        "code": 0,
+        "data": {
+            "originalURL": original_url,
+            "url": url
+        }
+    })))
 }
