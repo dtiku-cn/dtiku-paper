e0d10b31d5ac34d3f5e84809b23d167b7e65b304
holmofy <hff1996723@163.com>
2025-10-23 12:56:54 +0800

    add reset.sql where clause

--- sql/reset.sql
+++ sql/reset.sql
@@ -1,4 +1,4 @@
-update label set target_id=null;
-update paper set target_id=null;
-update question set target_id=null;
-update material set target_id=null;
+update label set target_id=null where from_ty = 'huatu';
+update paper set target_id=null where from_ty = 'huatu';
+update question set target_id=null where from_ty = 'huatu';
+update material set target_id=null where from_ty = 'huatu';
