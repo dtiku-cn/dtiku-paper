035bc8a8feadbecff29ab0b45c11e78a114c1500
holmofy <hff1996723@163.com>
2025-10-27 17:32:29 +0800

    fix huatu_sync

--- dtiku-backend/src/jobs/huatu_sync.rs
+++ dtiku-backend/src/jobs/huatu_sync.rs
@@ -985,7 +985,7 @@ impl OriginQuestion {
                     "单选选择题" | "单选题" | "单项选择题" | "选择题" | "阅读理解题"/*英语*/ => {
                         let answer_str = answer_list.clone().unwrap_or_default();
                         let answer = serde_json::from_str::<Vec<String>>(&answer_str)
-                            .with_context(||format!("parse answer_list failed: {answer_str}"))?
+                            .with_context(||format!("parse answer_list for Vec<String> failed: {answer_str}"))?
                             .into_iter()
                             .map(|i|i.parse())
                             .collect::<Result<Vec<u8>, ParseIntError>>()
@@ -1002,7 +1002,7 @@ impl OriginQuestion {
                     "多选题" | "多项选择题" | "双选题" | "M选N选择题" => {
                         let answer_str = answer_list.clone().unwrap_or_default();
                         let answer = serde_json::from_str::<Vec<String>>(&answer_str)
-                            .with_context(||format!("parse answer_list failed: {answer_str}"))?
+                            .with_context(||format!("parse answer_list for Vec<String> failed: {answer_str}"))?
                             .into_iter()
                             .map(|i|i.parse())
                             .collect::<Result<Vec<u8>, ParseIntError>>()
@@ -1020,7 +1020,7 @@ impl OriginQuestion {
                     "不定项选择题" | "案例选择题"| _ => {
                         let answer_str = answer_list.clone().unwrap_or_default();
                         let answer = serde_json::from_str::<Vec<String>>(&answer_str)
-                            .with_context(||format!("parse answer_list failed: {answer_str}"))?
+                            .with_context(||format!("parse answer_list for Vec<String> failed: {answer_str}"))?
                             .into_iter()
                             .map(|i|i.parse())
                             .collect::<Result<Vec<u8>, ParseIntError>>()
@@ -1038,7 +1038,7 @@ impl OriginQuestion {
             } else {
                 let answer_str = answer_list.clone().unwrap_or_default();
                 let answer = serde_json::from_str::<Vec<String>>(&answer_str)
-                    .with_context(|| format!("parse answer_list failed: {answer_str}"))?
+                    .with_context(|| format!("parse answer_list for Vec<String> failed: {answer_str}"))?
                     .into_iter()
                     .map(|i| i.parse())
                     .collect::<Result<Vec<u8>, ParseIntError>>()
@@ -1058,7 +1058,7 @@ impl OriginQuestion {
             None => {
                 let solution = answer_list.clone().unwrap_or_default();
                 let solutions = serde_json::from_str::<Vec<AnswerListItem>>(&solution)
-                    .with_context(|| format!("parse answer_list failed: {solution}"))?
+                    .with_context(|| format!("parse answer_list to Vec<AnswerListItem> failed: [{solution}]"))?
                     .iter()
                     .map(|i| i.to_string())
                     .join("<br/><hr/><br/>");
@@ -1082,7 +1082,7 @@ impl OriginQuestion {
                 "单选选择题" | "单选题" | "单项选择题" | "选择题" | "阅读理解题"/*英语*/ => {
                     let answer_str = answer_list.clone().unwrap_or_default();
                     let answer = serde_json::from_str::<Vec<String>>(&answer_str)
-                        .with_context(||format!("parse answer_list failed: {answer_str}"))?
+                        .with_context(||format!("parse answer_list for Vec<String> failed: {answer_str}"))?
                         .into_iter()
                         .map(|i|i.parse())
                         .collect::<Result<Vec<u8>, ParseIntError>>()
@@ -1099,7 +1099,7 @@ impl OriginQuestion {
                 "多选题" | "多项选择题" | "双选题" | "M选N选择题" => {
                     let answer_str = answer_list.clone().unwrap_or_default();
                     let answer = serde_json::from_str::<Vec<String>>(&answer_str)
-                        .with_context(||format!("parse answer_list failed: {answer_str}"))?
+                        .with_context(||format!("parse answer_list for Vec<String> failed: {answer_str}"))?
                         .into_iter()
                         .map(|i|i.parse())
                         .collect::<Result<Vec<u8>, ParseIntError>>()
@@ -1117,7 +1117,7 @@ impl OriginQuestion {
                 "不定项选择题" | "案例选择题" => {
                     let answer_str = answer_list.clone().unwrap_or_default();
                     let answer = serde_json::from_str::<Vec<String>>(&answer_str)
-                        .with_context(||format!("parse answer_list failed: {answer_str}"))?
+                        .with_context(||format!("parse answer_list for Vec<String> failed: {answer_str}"))?
                         .into_iter()
                         .map(|i|i.parse())
                         .collect::<Result<Vec<u8>, ParseIntError>>()
