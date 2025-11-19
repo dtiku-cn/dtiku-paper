c8478280c0144c58d5e73f0236d722a1e782ed0a
holmofy <hff1996723@163.com>
2025-11-19 23:10:22 +0800

    支持用户id

--- dtiku-base/src/query/mod.rs
+++ dtiku-base/src/query/mod.rs
@@ -4,6 +4,7 @@ use sea_orm::{sea_query::IntoCondition, ColumnTrait};
 
 #[derive(Debug, Clone, serde::Deserialize, serde::Serialize)]
 pub struct UserQuery {
+    id: Option<i32>,
     name: Option<String>,
     expired: Option<bool>,
 }
@@ -12,6 +13,9 @@ impl IntoCondition for UserQuery {
     fn into_condition(self) -> sea_orm::Condition {
         let mut condition = sea_orm::Condition::all();
 
+        if let Some(id) = self.id {
+            condition = condition.add(user_info::Column::Id.eq(id));
+        }
         if let Some(name) = self.name {
             condition = condition.add(user_info::Column::Name.contains(name));
         }
